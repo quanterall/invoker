08ae52d37b1053fa2a3accd94fb5b2d6272389b0
Rickard Andersson <gonz@severnatazvezda.com>
2021-12-30 17:38:32 +0000

    upgrade `qtility`
    Environment code is now using throws instead of `Either`

--- src/Run.hs
+++ src/Run.hs
@@ -5,7 +5,6 @@ import Import
 import Network.AWS.QAWS (loadAWSEnvironment)
 import qualified Network.AWS.QAWS.SQS as SQS
 import Network.AWS.QAWS.SQS.Types
-import Qtility.Data (hush)
 import Qtility.Environment (readEnvironmentVariable)
 import Qtility.Environment.Types
 import RIO.FilePath ((</>))
@@ -14,36 +13,28 @@ import qualified UI
 
 run :: Options -> RIO App ()
 run Options {defaultQueueUrl, environmentFile} = do
-  awsEnv' <- liftIO $ loadAWSEnvironment environmentFile
+  env <- liftIO $ loadAWSEnvironment environmentFile
   _eventChannel <- liftIO $ newBChan 20
+  templates' <- liftIO Templates.loadTemplates `catchIO` \_ -> pure []
+  defaultQueueUrlFromFile <- (Just <$> liftIO loadQueueUrlFromFile) `catchIO` const (pure Nothing)
+  defaultQueueUrlFromEnv <-
+    (Just <$> liftIO (readEnvironmentVariable $ EnvironmentKey "QUEUE_URL"))
+      `catchAny` const (pure Nothing)
+  let defaultUrl = defaultQueueUrl <|> defaultQueueUrlFromEnv <|> defaultQueueUrlFromFile
+  currentQueueUrlRef <- liftIO $ newTVarIO defaultUrl
+  _queueAttributeThread <- liftIO $
+    async $
+      forever $ do
+        currentQueueUrl <- readTVarIO currentQueueUrlRef
+        case currentQueueUrl of
+          Nothing -> pure ()
+          Just queueUrl -> do
+            response <- SQS.getQueueAttributes' env queueUrl
+            let maybeAttributes = either (const Nothing) Just response
+            writeBChan _eventChannel (UI.CurrentQueueAttributes maybeAttributes)
+        threadDelay $ 2 * 1000 * 1000
+  liftIO $ UI.startUI currentQueueUrlRef _eventChannel env defaultUrl templates'
 
-  case awsEnv' of
-    Left e -> do
-      logError $ displayShow e
-      exitFailure
-    Right env -> do
-      templates' <- liftIO Templates.loadTemplates `catchIO` \_ -> pure []
-      defaultQueueUrlFromFile <- liftIO loadQueueUrlFromFile
-      defaultQueueUrlFromEnv <-
-        hush <$> liftIO (readEnvironmentVariable $ EnvironmentKey "QUEUE_URL")
-      let defaultUrl =
-            defaultQueueUrl
-              <|> defaultQueueUrlFromEnv
-              <|> either (const Nothing) Just defaultQueueUrlFromFile
-      currentQueueUrlRef <- liftIO $ newTVarIO defaultUrl
-      _queueAttributeThread <- liftIO $
-        async $
-          forever $ do
-            currentQueueUrl <- readTVarIO currentQueueUrlRef
-            case currentQueueUrl of
-              Nothing -> pure ()
-              Just queueUrl -> do
-                response <- SQS.getQueueAttributes' env queueUrl
-                let maybeAttributes = either (const Nothing) Just response
-                writeBChan _eventChannel (UI.CurrentQueueAttributes maybeAttributes)
-            threadDelay $ 2 * 1000 * 1000
-      liftIO $ UI.startUI currentQueueUrlRef _eventChannel env defaultUrl templates'
-
-loadQueueUrlFromFile :: IO (Either IOException QueueUrl)
+loadQueueUrlFromFile :: IO QueueUrl
 loadQueueUrlFromFile = do
-  fmap QueueUrl <$> tryIO (readFileUtf8 (".invoker" </> "queue"))
+  QueueUrl <$> readFileUtf8 (".invoker" </> "queue")
